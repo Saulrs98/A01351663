dd4e3cd44bf6df3e2b05d29ef9ea79488d6a6d11
Saulrs98 <62858470+Saulrs98@users.noreply.github.com>
2021-05-07 01:19:04 -0500

    html dinamico

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const bodyParser = require('body-parser');
 
 const path = require('path');
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
--- routes/jugadores.js
+++ routes/jugadores.js
@@ -14,7 +14,30 @@ app.post('/nuevo-jugador', (request, response, next) => {
 });
 
 router.get('/', (request, response, next) => {
-    response.sendFile(path.join(__dirname, '..', 'views', 'jugadores.html'));
+    //response.sendFile(path.join(__dirname, '..', 'views', 'jugadores.html'));
+    const jugadores = [
+        {
+            nombre: 'Cristiano Ronaldo',
+            descripcion: '5 balones de oro',
+            imagen: 'https://www.ecestaticos.com/image/clipping/73bb9e27fe7776087c497d4bd2f3a7b1/la-obsesion-de-cristiano-ronaldo-para-llegar-fresco-a-los-40-anos-su-rival-es-ibrahimovic.jpg'
+        },
+        {
+            nombre: 'Messi',
+            descripcion: '6 balones de oro',
+            imagen: 'https://ep01.epimg.net/deportes/imagenes/2019/04/19/actualidad/1555686393_895375_1555686917_noticia_normal.jpg'
+        },
+        {
+            nombre: 'Ronaldinho',
+            descripcion: '1 balon de oro',
+            imagen: 'https://www.fcbarcelonanoticias.com/uploads/s1/11/97/73/0/ronaldinho-celebrando-un-gol.jpeg'
+        },
+        {
+            nombre: 'Ronaldo Nazario',
+            descripcion: '2 balones de oro',
+            imagen: 'https://www.conmebol.com/sites/default/files/r9.jpg'
+        }
+    ];
+    response.render('jugadores', { jugadores: jugadores});
 });
 
 module.exports = router;  
